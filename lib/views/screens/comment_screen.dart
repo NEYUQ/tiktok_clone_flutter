673f0636f9994732ca42d9c3518752318f9a9413
TRUONGQUYEN <kymonos1@gmail.com>
2023-03-08 10:04:32 +0700

    update

---
+++ lib/views/screens/comment_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CommentScreen extends StatelessWidget {
+  const CommentScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("Display Comments"),
+      ),
+    );
+  }
+}
